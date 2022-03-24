c4a399ee5b6e29d7b92303032dd4bb76ef2cbfd8
Emmanuel Ohayon <eohayon@mail.ks.int>
2022-03-24 15:32:39 +0100

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='rsfstat',
-    version='0.1.0',
+    version='1.0.0',
     description='Compute statistics on an ASTERIOS static scheduling plan (RSF)',
 
     long_description=long_description,
